408702b34416a32cdf207e43cbef45a56a12126d
oNavShaHo <106837111+oNavShaHo@users.noreply.github.com>
2024-03-28 00:02:20 +0530

    finished

--- src/components/HealthComponent.js
+++ src/components/HealthComponent.js
@@ -10,23 +10,28 @@ const HealthComponent = (props) => {
   if (!healthData) {
     return <div>Error: Data not found.</div>;
   }
+  var c=healthData.img;
+  console.log(c);
   
   return (
    
     <div className="border-red-200 border w-[80vw] p-10 flex justify-between bg-green-100 text-green-800">
       <div>
-      <h1>Disease:<span className='font-bold'>{healthData.result.name}</span></h1>
+      <h1>Disease:<span className='font-bold'>{healthData.name}</span></h1>
       <p className='font-semibold'>Cure</p>
       <ul className='list-inside list-disc'>
       {
-        healthData.result.preventionSteps.map((e)=>(
+        healthData.preventionSteps.map((e)=>(
           <li>{e}</li>
         ))
       }
       </ul>
     </div>
     <div>
-    {/* <img src={props.img} alt={props.img.name} /> */}
+    {/* <img src="
+    
+    https://drive.google.com/uc?id=1Ecl9Fd-Z3e9MIlvY2QIfHiEuy9BKT5Tk
+   " alt={props.img.name} /> */}
     </div>
     </div>
    );
--- src/components/form.js
+++ src/components/form.js
@@ -1,83 +1,80 @@
-import React from 'react'
-import { useState } from 'react'
-import Navbar from './navbar';
-import axios from 'axios';
-import Ans from './Ans';
-
-
+import React, { useState } from "react";
+import { BrowserRouter as Router,  Link } from 'react-router-dom';
+import axios from "axios";
+import logo from "../assests/logo.png";
+import Ans from "./Ans";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import data from "../data/data.json";
 export default function Form() {
-  const [dd,setDD]=useState();
-  console.log(setDD);
+  const [dd, setDD] = useState(null);
   const [selectedImage, setSelectedImage] = useState(null);
 
-   
-   function fetchh()
-  {
-      
-    axios.post('http://localhost:3000/img', {
-       'im':selectedImage,
-      
-    })
-    .then(function (response) {
-      console.log("response:",response);
-    //   setDD(response.data);
-     })
-    .catch(function (error) {
-      console.log(error);
-    });
-     
-  
-  }
+  const fetchh = () => {
+    const formData = new FormData();
+    formData.append("file", selectedImage);
 
-var file=null;
+    axios
+      .post("https://farmora.onrender.com/predict", formData)
+      .then(function (response) {
+        console.log("response:", response);
+        setDD(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   const handleImageChange = (e) => {
-     file = e.target.files[0];
+    console.log(e);
+    const file = e.target.files[0];
+    console.log(file);
     if (file) {
-      // You can perform additional validation here if needed
       setSelectedImage(file);
     }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle the image submission logic here, for example, sending it to a server or processing it.
     if (selectedImage) {
-      console.log('Selected Image:', selectedImage);
-        
-        fetchh();
-      
-
+      fetchh();
     } else {
-      console.log('No image selected');
+      console.log("No image selected");
     }
   };
-  
-  
-    
-  
 
   return (
-    <div  className=''>
-       <Navbar/>
-     <div className='flex flex-col gap-10 justify-center items-center border  border-red-300 border-r-4 p-10 '>
-      <h1 className='font-bold text-4xl text-[#023F3A]'>Crop Health Detector</h1>
-           {!dd?(<form className='border gap-10 flex flex-col justify-center item-center  p-10' onSubmit={handleSubmit}>
-      <div className='flex  justify-center item-center '>
-        <label htmlFor="imageInput">Choose: :</label>
-        <input
-          type="file"
-          id="imageInput"
-          accept="image/*" // Specify that only image files are allowed
-          onChange={handleImageChange}
-        />
+    <div className="">
+      <div className="flex justify-between ml-2 mr-2 items-center h-[4rem]">
+        <img className="h-[3rem]" src={logo} alt="logo" />
+
+      <Link to='/'>< ArrowBackIcon  /> </Link> 
       </div>
-      <button className='bg-[#023F3A]  text-white' type="submit">Submit</button>
-    </form>):(
-      <Ans dataa={dd} img={file}/>
-    )}
-     
-     </div>
- 
+      <div className="flex flex-col gap-10 justify-center items-center border border-red-300 border-r-4 p-10">
+        <h1 className="font-bold text-4xl text-[#023F3A]">
+          Crop Health Detector
+        </h1>
+        {!dd ? (
+          <form
+            className="border gap-10 flex flex-col justify-center item-center  p-10"
+            onSubmit={handleSubmit}
+          >
+            <div className="flex justify-center item-center ">
+              <label htmlFor="imageInput">Choose:</label>
+              <input
+                type="file"
+                id="imageInput"
+                accept="image/*"
+                onChange={handleImageChange}
+              />
+            </div>
+            <button className="bg-[#023F3A] text-white" type="submit">
+              Submit
+            </button>
+          </form>
+        ) : (
+          <Ans dataa={data[dd.class]} img={selectedImage} />
+        )}
+      </div>
     </div>
-  )
+  );
 }
